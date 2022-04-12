06f949e4da33087d530d6ead4a982b528d5cea18
Chih-Chen Kao <ChihChen.Kao@amd.com>
2022-04-12 13:55:07 +0200

    Refactor the code to add the basic constructs to support selecting different scan algorithms. Add different implementation of the scan algorithm: CPU, single WG and all WG .
    Signed-off-by: Chih-Chen Kao <ChihChen.Kao@amd.com>

--- Test/ParallelPrimitives/RadixSort.h
+++ Test/ParallelPrimitives/RadixSort.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Orochi/Orochi.h>
+#include <string>
+#include <unordered_map>
 
 namespace Oro
 {
@@ -12,26 +14,50 @@ class RadixSort
 
 	enum Flag
 	{
-		FLAG_LOG = 1<<0,
+		FLAG_LOG = 1 << 0,
 	};
 
-	  RadixSort();
+	RadixSort();
 
-	  ~RadixSort();
+	void configure( oroDevice device, u32& tempBufferSizeOut );
 
-	  void configure( oroDevice device, u32& tempBufferSizeOut );
+	void setFlag( Flag flag );
 
-	  void setFlag( Flag flag );
-
-	  void sort( u32* src, u32* dst, int n, int startBit, int endBit, u32* tempBuffer );
+	void sort( u32* src, u32* dst, int n, int startBit, int endBit, u32* tempBuffer );
 
   private:
-	  void sort1pass( u32* src, u32* dst, int n, int startBit, int endBit, int* tmps );
+	void sort1pass( u32* src, u32* dst, int n, int startBit, int endBit, int* tmps );
+
+	void compileKernels();
 
   private:
-	  int m_nWGsToExecute;
-	  Flag m_flags;
+	int m_nWGsToExecute;
+	Flag m_flags;
 
+	enum class Kernel
+	{
+		COUNT,
+		COUNT_REF,
+		SCAN_SINGLE_WG,
+		SCAN_PARALLEL,
+		APPLY_OFFSET,
+		SORT,
+		SORT_REF
+	};
+
+	std::unordered_map<Kernel, oroFunction> oroFunctions;
+
+	/// @brief  The enum class which indicates the selected algorithm of prefix scan.
+	enum class ScanAlgo
+	{
+		SCAN_CPU,
+		SCAN_GPU_SINGLE_WG,
+		SCAN_GPU_PARALLEL,
+	};
+
+	constexpr static auto selectedScanAlgo{ ScanAlgo::SCAN_GPU_PARALLEL };
+
+	int* m_partialSum{ nullptr };
 };
 
-};
+}; // namespace Oro
